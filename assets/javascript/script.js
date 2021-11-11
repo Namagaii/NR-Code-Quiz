09d13c0d741c0c70b60018ee45b2c94b5ba2d4a4
namagaii <rnoel2125@gmail.com>
2021-11-10 20:59:27 -0500

    Started planning and psudo coding the website.

---
+++ assets/javascript/script.js
@@ -0,0 +1,35 @@
+/*
+    Notes:
+    Clear #Answers and write new questions
+    Listen for form submition formEl.on("submit", function)
+    event.preventDefault() on form
+
+    When Game Starts:
+    Set timer and start countdown
+    Show Question
+*/
+
+/*
+get start button
+set start button event listener
+function{
+    hide #HomeSection
+    show #QuizSection
+    displayNextQuestion();
+}
+*/
+
+/*
+    questions[] = [...]
+    answers[] = [[],[],[]]
+    questions[] = [
+        {
+            question: "question?!"
+            answers: [...]
+            correctAnswer: 
+        }
+    ]
+    function displayNextQuestion(){
+
+    }
+*/
